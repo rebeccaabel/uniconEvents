bd79d706587864981f62a35e904c3c41f1a8f5cc
Martin Skagh <martin.skagh@gmail.com>
2023-01-19 11:32:28 +0100

    feature done
    takes the artistArray and sorts it by dateAdded and prints out the first 10 results

--- client/src/components/mostRecentEvents.jsx
+++ client/src/components/mostRecentEvents.jsx
@@ -1,78 +1,28 @@
 import Card from "./Card.jsx";
-
+import { artists } from "./artistArray.jsx";
+let artistsSortedByDateAdded = []
+let numberOfCardsShown = 9;
 export default function () {
-    const artists = [
-        {
-            title: "Taylor Swift",
-            text: `This vegetarian, vegan and gluten-free mushroom burger recipe may just surprise you with how tasty it is. While mushrooms provide plenty of flavor and texture, the addition of pinto beans makes this veggie burger recipe high in fiber and protein too. There's not much to them, other than that! Just mash everything up together, form into patties, heat and serve!`,
-            image: 'https://images.unsplash.com/photo-1532768641073-503a250f9754?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=bceece29b062fe6d758ddfa2400c554c&auto=format&w=1024&q=80',
-            dateAdded: 20230120,
-        },
-        {
-            title: "Harry Styles",
-            text: `Mushrooms are surprisingly similar in taste and texture to meat when cooked, and this mushroom veggie burger recipe may just surprise you with how tasty it is. While mushrooms provide plenty of flavor and texture, the addition of pinto beans makes this recipe high in fiber and protein too. `,
-            image: 'https://veryveganrecipes.com/wp-content/uploads/2015/12/how-to-make-vegan-portobello-mushroom-burger-recipe.png',
-            dateAdded: 20230114,
-        },
-        {
-            title: "Kayne West",
-            text: `Mushrooms are surprisingly similar in taste and texture to meat when cooked, and this mushroom veggie burger recipe may just surprise you with how tasty it is. While mushrooms provide plenty of flavor and texture, the addition of pinto beans makes this recipe high in fiber and protein too. `,
-            image: 'https://veryveganrecipes.com/wp-content/uploads/2015/12/how-to-make-vegan-portobello-mushroom-burger-recipe.png',
-            dateAdded: 20230115,
-        },
-        {
-            title: "Kimberly Amethyst",
-            text: `Mushrooms are surprisingly similar in taste and texture to meat when cooked, and this mushroom veggie burger recipe may just surprise you with how tasty it is. While mushrooms provide plenty of flavor and texture, the addition of pinto beans makes this recipe high in fiber and protein too. `,
-            image: 'https://veryveganrecipes.com/wp-content/uploads/2015/12/how-to-make-vegan-portobello-mushroom-burger-recipe.png',
-            dateAdded: 20230116,
-        },
-        {
-            title: "Kimberly Amethyst",
-            text: `Mushrooms are surprisingly similar in taste and texture to meat when cooked, and this mushroom veggie burger recipe may just surprise you with how tasty it is. While mushrooms provide plenty of flavor and texture, the addition of pinto beans makes this recipe high in fiber and protein too. `,
-            image: 'https://veryveganrecipes.com/wp-content/uploads/2015/12/how-to-make-vegan-portobello-mushroom-burger-recipe.png',
-            dateAdded: 20230116,
-        },
-        {
-            title: "Kimberly Amethyst",
-            text: `Mushrooms are surprisingly similar in taste and texture to meat when cooked, and this mushroom veggie burger recipe may just surprise you with how tasty it is. While mushrooms provide plenty of flavor and texture, the addition of pinto beans makes this recipe high in fiber and protein too. `,
-            image: 'https://veryveganrecipes.com/wp-content/uploads/2015/12/how-to-make-vegan-portobello-mushroom-burger-recipe.png',
-            dateAdded: 20230116,
-        },
-        {
-            title: "Kimberly Amethyst",
-            text: `Mushrooms are surprisingly similar in taste and texture to meat when cooked, and this mushroom veggie burger recipe may just surprise you with how tasty it is. While mushrooms provide plenty of flavor and texture, the addition of pinto beans makes this recipe high in fiber and protein too. `,
-            image: 'https://veryveganrecipes.com/wp-content/uploads/2015/12/how-to-make-vegan-portobello-mushroom-burger-recipe.png',
-            dateAdded: 20230117,
-        },
-        {
-            title: "Kimberly Amethyst",
-            text: `Mushrooms are surprisingly similar in taste and texture to meat when cooked, and this mushroom veggie burger recipe may just surprise you with how tasty it is. While mushrooms provide plenty of flavor and texture, the addition of pinto beans makes this recipe high in fiber and protein too. `,
-            image: 'https://veryveganrecipes.com/wp-content/uploads/2015/12/how-to-make-vegan-portobello-mushroom-burger-recipe.png',
-            dateAdded: 20230118,
-        },
-        {
-            title: "Kimberly Amethyst",
-            text: `Mushrooms are surprisingly similar in taste and texture to meat when cooked, and this mushroom veggie burger recipe may just surprise you with how tasty it is. While mushrooms provide plenty of flavor and texture, the addition of pinto beans makes this recipe high in fiber and protein too. `,
-            image: 'https://veryveganrecipes.com/wp-content/uploads/2015/12/how-to-make-vegan-portobello-mushroom-burger-recipe.png',
-            dateAdded: 20230119,
-        },
-        {
-            title: "Kimberly Amethyst",
-            text: `Mushrooms are surprisingly similar in taste and texture to meat when cooked, and this mushroom veggie burger recipe may just surprise you with how tasty it is. While mushrooms provide plenty of flavor and texture, the addition of pinto beans makes this recipe high in fiber and protein too. `,
-            image: 'https://veryveganrecipes.com/wp-content/uploads/2015/12/how-to-make-vegan-portobello-mushroom-burger-recipe.png',
-            dateAdded: 20230120,
+    let sortingNumber = 0
+    for (let i = 0; i < artists.length; i++) {
+        if (artistsSortedByDateAdded.length > numberOfCardsShown) {
+            continue
         }
-    ];
-
-    const sortedArr = artists
-
+        if (artists[i].dateAdded > sortingNumber) {
+            artistsSortedByDateAdded.unshift(artists[i])
+            sortingNumber = artists[i].dateAdded
+        } else {
+            artistsSortedByDateAdded.push(artists[i])
+            sortingNumber = artists[i].dateAdded
+        }
+    }
 
     return <div className="card-area-wrapper">
         <h2>Newly Added Events</h2>
         <div className="card-area">
-        {
-            artists.map(artist => <Card title={artist.title} text={artist.text} image={artist.image} />)
-        }
+            {
+                artistsSortedByDateAdded.map(artist => <Card title={artist.title} text={artist.text} image={artist.image} />)
+            }
         </div>
     </div>
 
