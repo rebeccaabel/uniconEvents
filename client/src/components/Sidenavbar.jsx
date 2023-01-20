84669fcc69ee5c330c6d60a1a8859cdc1edbf483
Markus Andersson <erimarandersson@gmail.com>
2023-01-20 10:03:53 +0100

    added css to profile page

--- client/src/components/Sidenavbar.jsx
+++ client/src/components/Sidenavbar.jsx
@@ -5,13 +5,15 @@ export default function () {
     const [displayinfo, setinfo] = useState();
     useEffect(() => {
         setinfo('');
+
+        //let settingbtn = document.querySelector(".contact-information-container");
     },[])
 
     return <>
         <div className='side-navbar-wrapper'>
             <div className='side-navbar'>
                 <div>
-                    <button onClick={() => setinfo(Usersettings)}> Settings </button>
+                    <button className="setting-btn" onClick={() => setinfo(Usersettings)}> Settings </button>
                     <button>My tickets</button>
                     <button>Calender</button>
                 </div>
