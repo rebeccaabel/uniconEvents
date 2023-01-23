12d7397c9c5cc384cd0d884a3647275c3ad266db
Rebecca Abel <rebeccaabel@hotmail.se>
2023-01-23 15:59:13 +0100

    cleaned up code

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,22 +1,13 @@
 import {Link} from "react-router-dom";
 import profile2 from "../images/profile2.png"
-import logoHeader from "../images/LogoPurpleBlack.png"
+import LogoHeader from "../images/LogoPurpleBlack.png"
 import Navbar from "./Navbar.jsx";
 
 
 export default function () {
-    return <div className='header-wrapper'>
-            <header>
-                    <img id="logoheader" src={logoHeader}/>
-                <div className='top-navbar'>
-                    <Navbar/>
-                </div>
-
-
-                <Link to="profile"><img id ="profileheader" src={profile2}/></Link>
-            </header>
-
-        </div>
-
-
+    return <header>
+        <img id="logoheader" src={LogoHeader}/>
+            <Navbar/>
+        <Link to="profile"><img id="profileheader" src={profile2}/></Link>
+    </header>
 }
