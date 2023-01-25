2c80334651c2d975011e08ea828c818dbb2a3c72
Markus Andersson <erimarandersson@gmail.com>
2023-01-25 19:43:48 +0100

    Added display of months

--- client/src/components/MyCalender.jsx
+++ client/src/components/MyCalender.jsx
@@ -1,13 +1,21 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 export default function () {
+    const [variousMonths, setMonths] = useState([handleMonth()]);
+    let displayNextMonth = () => {
+        setMonths(() => ["Next is" + nextMonth()])
+    }
+    let displayPrevMonth = () => {
+        setMonths(() => ["Prev is" + prevMonth()])
+    }
     return <>
         <div>
 
-            <p className='date'>"Display current month"</p>
+            <p className={"month"}>{variousMonths}</p>
+            <button className="prev" onClick={() => setMonths(displayPrevMonth)}>Click for previous month</button>
+            <button className="next-month" onClick={() => setMonths(displayNextMonth)}>Click for next month</button>
             <p className="saved-date">Possible saved events here</p>
-            <button className="next-month" onClick={nextMonth}>Click for next month</button>
-            <button className="next-month" onClick={prevMonth}>Click for prev month</button>
+
 
         </div>
 
@@ -15,12 +23,12 @@ export default function () {
     </>
 }
 const months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+
 export function handleMonth(){
     //todo: Given i am on the profilepage When i click on my calender.
     // Then I can see a scrolldown menu with my saved events for each month
 
     let savedEvents = "24/1";
-    console.log(months)
     let dates = new Date();
     let currentMonth = months[dates.getMonth()];
     console.log(currentMonth)
@@ -29,15 +37,16 @@ export function handleMonth(){
         console.log(savedEvents)
     }
 
+    return currentMonth;
 }
 function nextMonth(){
     let dates = new Date();
     let monthIncrement = months[dates.getMonth() + 1];
-    console.log(monthIncrement)
+    return monthIncrement;
 
 }
 function prevMonth(){
     let dates = new Date();
     let monthDecrement = months[dates.getMonth() - 1];
-    console.log(monthDecrement)
+    return monthDecrement;
 }
