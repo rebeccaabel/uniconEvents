1271bc703bfcfde68a6f8ca5fb4e804d84314d44
Rebecca Abel <rebeccaabel@hotmail.se>
2023-01-26 11:20:34 +0100

    fixed header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,6 +1,6 @@
 import {Link} from "react-router-dom";
 import profile2 from "../images/profile2.png"
-import LogoHeader from "../images/LogoPurpleBlack.png"
+import LogoHeader from "../images/Component 3home-button.png"
 import Navbar from "./Navbar.jsx";
 import Searchbar from "./Searchbar.jsx";
 
@@ -8,6 +8,7 @@ import Searchbar from "./Searchbar.jsx";
 export default function () {
     return <header>
         <img id="logoheader" src={LogoHeader}/>
+        <h1 id='header-title'>Unicorn Events</h1>
         <Searchbar/>
         <Navbar/>
         <Link to="profile"><img id="profileheader" src={profile2}/></Link>
