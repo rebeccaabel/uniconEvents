b52e66e57cc378c708e032a1584a94f14c1387a3
Martin Skagh <martin.skagh@gmail.com>
2023-01-27 14:13:16 +0100

    added a global variable
    woop woop! It works!

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,16 +5,33 @@ import './Paymentpage.css'
 import {Outlet} from "react-router-dom";
 import Header from "./components/Header.jsx";
 import Footer from "./components/Footer.jsx";
+import React from 'react';
 
+export const GlobalVariable = React.createContext({
+    value: 0,
+    changeValue: (input) => {
+        GlobalVariable._currentValue.value += input
+    }
+});
 
 export default function () {
+  return (
+    <GlobalVariable.Provider value="Reed">
+      <App />
+    </GlobalVariable.Provider>
+  )
+}
+
+function App() {
     return <>
         <Header/>
         <Outlet/>
         <Footer/>
     </>
+    }
+   
 
-}
+
 
 
 
