31419c4740f2e6777bf16f859b9dc26bf36a9239
CaptainMorgan1992 <Kimberly.Morgan1992@outlook.com>
2023-02-01 16:15:17 +0100

    Added refactoring to payment

--- client/src/components/CardDetails.jsx
+++ client/src/components/CardDetails.jsx
@@ -1,40 +1,22 @@
-
 import {artists} from "./artistArray.jsx";
 import {Link} from "react-router-dom";
+import DisplayArtistCardPayment from "../components/DisplayArtistCardPayment";
+import CalculatePricePaymentpage from "../components/CalculatePricePaymentpage.jsx";
+import FormPaymentDetails from "../components/FormPaymentDetails.jsx";
 
 
 export default function () {
 
     return <div className="payment-wrapper">
-
         <div className="payment-container">
-
-            <div className="event-details">
-                <h1> Event Details</h1>
-                <p>{artists[0].title}</p>
-                <p>{artists[0].location}</p>
-                <p>{artists[0].date.toLocaleDateString('sv-SE')}</p>
-
-            </div>
-
-            <img src={artists[0].image}></img>
-
+            <DisplayArtistCardPayment/>
             <div className="payment-details">
-
-                <p>Total:
-                    <label>
-                        200kr
-                    </label></p>
-                <form className="date-form"><p>Name on Card</p><input type="text" placeholder="Input name here"/></form>
-                <form className="date-form"><p>Card digits</p><input type="text" placeholder="Enter card number "/></form>
-
-                <form className="date-form"><p>Expiration date</p><input placeholder="mm/dd"/></form>
-                <form className="date-form"><p>CVC</p><input type="text" placeholder="123"/></form>
-                <input type="radio" /> Buy for a friend
-
+                <CalculatePricePaymentpage/>
+                <FormPaymentDetails/>
             </div>
-
-            <Link to={'/VerifiedPaymentPage'}><button> Purchase </button></Link>
+            <Link to={'/VerifiedPaymentPage'}>
+                <button> Purchase</button>
+            </Link>
         </div>
     </div>
 
---
+++ client/src/components/FormPaymentDetails.jsx
@@ -0,0 +1,10 @@
+export default function () {
+    return <>
+        <form className="date-form"><p>Name on Card</p><input type="text" placeholder="Input name here"/></form>
+        <form className="date-form"><p>Card digits</p><input type="text" placeholder="Enter card number "/></form>
+
+        <form className="date-form"><p>Expiration date</p><input placeholder="mm/dd"/></form>
+        <form className="date-form"><p>CVC</p><input type="text" placeholder="123"/></form>
+        <input type="radio"/> Buy for a friend
+    </>
+}
