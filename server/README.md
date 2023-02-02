ec1aca952054f03ad93f9e2dd81ea1c31f19e396
Martin Skagh <martin.skagh@gmail.com>
2023-02-02 11:56:21 +0100

    deleating bleeting

--- server/README.md
+++
@@ -1,1 +0,0 @@
-# live-fanatic-backend
--- server/modules/encrypt.js
+++
@@ -1,8 +0,0 @@
-import crypto from "crypto";
-
-// noinspection SpellCheckingInspection
-const salt = "asefhwuhvuaseowrm".toString('hex')
-
-export default function (password) { // utility att skapa kryperade lösenord
-  return crypto.pbkdf2Sync(password, salt, 1000, 64, `sha512`).toString('hex')
-}
