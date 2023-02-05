183a8bd040194c25422046bf5be02daad72c9767
Martin Skagh <martin.skagh@gmail.com>
2023-02-05 23:09:45 +0100

    Added dropdown menu with search function
    and a metric shit tonne of other stuff. Getting closer to done. Woop woop!
    
    Also merged main into branch to get ahead of the conflicts. That's bloody well it for today. Its been fun and all, but now I need my 30min of spare-time.

--- client/src/components/Filter.jsx
+++ client/src/components/Filter.jsx
@@ -1,128 +1,67 @@
 import { useContext, useRef, useState } from "react";
 import Card from "./EventCard";
-import { sortByMostRecentAdded } from "./artistArrayFunctions";
 import GlobalContext from "./GlobalContext";
-
+import "../styles/Dropdown.css"
+import Dropdown from "./FilterDropdown";
 
 export default function () {
     const { concerts } = useContext(GlobalContext)
-    let [sortedArray, setFilter] = useState([])
-    let startDate = useRef(null)
-    let endDate = useRef(null)
-    let cityButtonDataArray = [
-        {
-            buttonName: "Malmö",
-            isOn: false,
-            index: 0
-        }, {
-            buttonName: "Stockholm",
-            isOn: false,
-            index: 1
-        }, {
-            buttonName: "Göteborg",
-            isOn: false,
-            index: 2
-        }, {
-            buttonName: "Borås",
-            isOn: false,
-            index: 3
-        }, {
-            buttonName: "Skövde",
-            isOn: false,
-            index: 4
-        }
-    ]
-    let dateButtonDataArray = [
-        {
-            buttonName: 'Today',
-            isOn: false,
-            index: 0,
-        }, {
-            buttonName: 'Tomorrow',
-            isOn: false,
-            index: 1,
-        }, {
-            buttonName: 'Within 7 Days',
-            isOn: false,
-            index: 2,
-        }, {
-            buttonName: 'This Month',
-            isOn: false,
-            index: 3,
-        }, {
-            buttonName: 'This Year',
-            isOn: false,
-            index: 4,
-        }, {
-            buttonName: 'Next Year',
-            isOn: false,
-            index: 5,
-        }
-    ]
+    let [sortedArray, setSortedArray] = useState([])
+    const [selectedVenue, setSelectedVenue] = useState(null)
+    const [selectedBand, setSelectedBand] = useState(null)
+    const startDate = useRef(null)
+    const endDate = useRef(null)
+    
+    const venueOptions = concerts.map(a => a.venue)
+    let uniqueVenueOptions = venueOptions.filter((c, index) => {
+        return venueOptions.indexOf(c) === index
+    })
+    const bandOptions = concerts.map(a => a.name)
+    let uniqueBandOptions = bandOptions.filter((c, index) => {
+        return bandOptions.indexOf(c) === index
+    })
+
+    const selectBand = (band) => {
+        setSelectedBand(band)
+    }
+    const selectVenue = (venue) => {
+        setSelectedVenue(venue)
+    }
 
     function filterArray() {
         const today = new Date("SEP 17 - 07:00")
         let cloneConcerts = [...concerts]
-        let isToday = dateButtonDataArray[0].isOn
-        let isTomorrow = dateButtonDataArray[1].isOn
-        let isThisWeek = dateButtonDataArray[2].isOn
-        let isThisMonth = dateButtonDataArray[3].isOn
-        let isThisYear = dateButtonDataArray[4].isOn
-        let isNextYear = dateButtonDataArray[5].isOn
-        for (let i = cloneConcerts.length-1; i >= 0; i--){
-            const eventDate = new Date(cloneConcerts[i].datum)
-        if (isToday &&
-            eventDate.getDate() !== today.getDate() ||
-            eventDate.getMonth() !== today.getMonth() ||
-            eventDate.getFullYear() !== today.getFullYear()
-        ) {
-            cloneConcerts.splice(i,1)
-        } 
 
-         else if (isTomorrow &&
-            eventDate.getDate() == today.getDate() + 1 &&
-            eventDate.getMonth() == today.getMonth() &&
-            eventDate.getFullYear() == today.getFullYear()
-        ) {
-            cloneConcerts.splice(i,1)
+        for (let i = cloneConcerts.length - 1; i >= 0; i--) {
+            if (selectedBand &&
+                cloneConcerts[i].name !== selectedBand) {
+                cloneConcerts.splice(i,1)
+            }else if (selectedVenue &&
+                cloneConcerts[i].venue !== selectedVenue){
+                cloneConcerts.splice(i,1)
+            }/* 
+            
+            ************START WORKING ON THIS************
+            *****NEED TO REVERSE THE OUTCOME AND*********
+            ********CHECK IF THE STARTDATE AND***********
+            ***********ENDDATE VARIABLES HAVE************
+            *****************ANY INPUT*******************
+            else if (
+                cloneConcerts[i].date.getTime() > startDate.getTime() &
+                cloneConcerts[i].date.getTime() < endDate.getTime()) {
+                cloneConcerts.splice(i,1) 
         }
-
-        else if (isThisWeek &&
-            eventDate.getDate() == today.getDate() + 7 &&
-            eventDate.getMonth() == today.getMonth() &&
-            eventDate.getFullYear() == today.getFullYear()
-        ) {
-            cloneConcerts.splice(i,1)
+        */
         }
-
-        else if (isThisMonth &&
-            eventDate.getMonth() == today.getMonth() &&
-            eventDate.getFullYear() == today.getFullYear()
-        ) {
-            cloneConcerts.splice(i,1)
-        }
-
-        else if (isThisYear &&
-            eventDate.getFullYear() == today.getFullYear()
-        ) {
-            cloneConcerts.splice(i,1)
-        }
-
-        if (isNextYear &&
-            eventDate.getFullYear() == today.getFullYear() + 1
-        ) {
-            cloneConcerts.splice(i,1)
-        } 
+        return setSortedArray(sortedArray = cloneConcerts)
     }
-    return setFilter(sortedArray = cloneConcerts)
-}
 
     function mostRecentEventsFilter() {
         let cloneConcerts = [...concerts]
-        cloneConcerts.sort((a,b) => {
+        cloneConcerts.sort((a, b) => {
             return b.id - a.id
         })
-        setFilter(sortedArray = cloneConcerts.slice(0,10))
+        setFilter(sortedArray = cloneConcerts.slice(0, 10))
     }
 
     function DisplayResults() {
@@ -146,53 +85,10 @@ export default function () {
         return <button id="filter-page-search-button" onClick={() => { filterArray() }}>Search</button>
     }
 
-    function CityButton({ type, index }) {
-        const [active, setActive] = useState(false);
-        const handleClick = () => {
-            setActive(!active);
-            if (active) {
-                { cityButtonDataArray[index].isOn = false; }
-            } else { cityButtonDataArray[index].isOn = true };
-        };
-        return <button
-            onClick={() => { handleClick(); }}
-            style={{ backgroundColor: active ? "gray" : "black" }}
-        >{type}</button>
-    }
-
-    function CityButtonGroup() {
-        return <div className="city-filters">
-            {cityButtonDataArray.map(buttons => <CityButton type={buttons.buttonName} index={buttons.index}></CityButton>)}
-        </div>
-    }
-
-    function DateButton({ type, index }) {
-        const [active, setActive] = useState(false);
-        const handleClick = () => {
-            setActive(!active);
-            if (active) {
-                { dateButtonDataArray[index].isOn = false; }
-            } else { dateButtonDataArray[index].isOn = true };
-        };
-        return <button
-            onClick={() => { handleClick(); }}
-            style={{ backgroundColor: active ? "gray" : "black" }}
-        >{type}</button>
-    }
-
-    function DateButtonGroup() {
-        return <div className="date-filters">
-            {dateButtonDataArray.map(buttons => <DateButton type={buttons.buttonName} index={buttons.index}></DateButton>)}
-            <button onClick={mostRecentEventsFilter}>Recently added events</button>
-        </div>
-    }
-
-
-
     function EventFilter() {
         return <div className="filter-container">
-            <DateButtonGroup />
-            <CityButtonGroup />
+            <Dropdown isSearchable placeHolder="Select Venue..." options={uniqueVenueOptions} selection={selectVenue} />
+            <Dropdown isSearchable placeHolder="Select Band..." options={uniqueBandOptions} selection={selectBand}/>
             <TwoDatesFilter />
             <SearchButton />
         </div>
