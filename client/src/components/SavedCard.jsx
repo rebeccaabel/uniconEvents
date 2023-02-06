b85a036637eaa0e1cf0b0cb812a39f9a5c6bc19c
Erimara <erimarandersson@gmail.com>
2023-02-06 17:15:14 +0100

    added state to remove saved events

--- client/src/components/SavedCard.jsx
+++ client/src/components/SavedCard.jsx
@@ -1,12 +1,20 @@
+import {useState} from "react";
+
 export default function ({name, image, location,datum}) {
+    const [card, setCard] = useState(true)
     return <>
-        <div className="card" alt='artist picture' style={{backgroundImage: `url(${image})`}}>
+        {card &&( <div className="card" style={{backgroundImage: `url(${image})`}}>
+
             <div className="event-card-details">
                 <h3>{name}</h3>
                 <p>{location}</p>
                 <p>{datum}</p>
-            </div>
-        </div>
+                    <button className={"save-event"} onClick={() => {RemoveCard()} }
+                            style={{color: "red"}}><i className="fa-solid fa-heart"></i></button>
+            </div>)
+        </div> )}
     </>
-
+    function RemoveCard(){
+        setCard((removed) => !removed)
+    }
 }
