f332dd5ae620cf161ce83b8d3f1d4f81d21a1955
CaptainMorgan1992 <Kimberly.Morgan1992@outlook.com>
2023-02-07 11:48:16 +0100

    Added removal of searchbar, added css to mediaquery

--- client/src/components/Searchbar-navbar-component.jsx
+++ client/src/components/Searchbar-navbar-component.jsx
@@ -1,13 +1,7 @@
-import Searchbar from "./Searchbar.jsx";
 import Navbar from "./Navbar.jsx";
 
 export default function () {
-    return <div className="searchbar-and-navbar-wrapper">
-        <div className="search-header">
-            <Searchbar />
-        </div>
-        <div className="nav-header">
+    return <div className="nav-header">
             <Navbar />
         </div>
-    </div>
 }
