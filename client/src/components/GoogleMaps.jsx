84b5f63f520ca0197f28195c2c19614149ac3ba5
KilRoy95 <75723624+KilRoy95@users.noreply.github.com>
2023-02-07 15:54:04 +0100

    Added Google API to Map-Component

--- client/src/components/GoogleMaps.jsx
+++ client/src/components/GoogleMaps.jsx
@@ -1,8 +1,17 @@
 import "../styles/GoogleMaps.css";
-
-let location = "sjöbo";
+import GlobalContext from "./GlobalContext.jsx";
+import {useParams} from "react-router-dom";
+import {useContext} from "react";
 
 export default function () {
-    return <iframe src={`https://www.google.com/maps/embed/v1/place?key=AIzaSyBFTOXrCK1OfYXLRLXdCK70_U9w5rlqXEY&q=${location}`}></iframe>
+
+    const {concerts} = useContext(GlobalContext);
+    const id = useParams().id
+    const concert = concerts.find(c => c.id === parseInt(id))
+
+    if (!concert) return null
+    const {venue} = concert
+
+    return <iframe src={`https://www.google.com/maps/embed/v1/place?key=AIzaSyBFTOXrCK1OfYXLRLXdCK70_U9w5rlqXEY&q=${venue}`}></iframe>
 
 }
