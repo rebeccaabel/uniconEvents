6203ada02c117c1c0dff46d037c229bc1b01d6f3
Erimara <erimarandersson@gmail.com>
2023-02-09 14:53:51 +0100

    added "qr code" and visiblity to print button

--- client/src/components/paymentComponents/PaymentVerifiedTicket.jsx
+++ client/src/components/paymentComponents/PaymentVerifiedTicket.jsx
@@ -7,16 +7,18 @@ import LocationArtistHub from "../eventComponents/LocationEventHub.jsx";
 import ImageArtistHub from "../eventComponents/ImageEventHub.jsx";
 import CalculatePricePaymentpage from "./CalculatePricePaymentpage.jsx";
 
-function VerifiedCard(props) {
+function VerifiedCard() {
     return (
         <div className="verified-ticket">
                 <h1>Thank you for your purchase!</h1>
+            <h2>Your Ticket</h2>
+            <img style={{width:"50px", height:"50px"}} src="https://api.qrserver.com/v1/create-qr-code/?data=HelloWorld&amp;size=100x100" alt="" title="" />
                 <CalculatePricePaymentpage/>
                 <ImageArtistHub/>
             <div className="details">
                 <TitleArtistHub/>
                 <LocationArtistHub/>
-                <button onClick={printTicket}></button>
+                <button style={{width:"50px", height:"50px", background:"grey"}} onClick={printTicket}>Print out ticket</button>
             </div>
             <Map/>
         </div>
