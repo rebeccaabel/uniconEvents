a14091ec0f4779f4722579ae4a81267dfe189630
Martin Skagh <martin.skagh@gmail.com>
2023-02-09 22:23:25 +0100

    Finalized
    last minute bugfixing (date-picker gave faulty return)
    css - cleanup and finishing touches
    refactoring - one final look

--- client/src/components/filterComponents/BandOptions.js
+++ client/src/components/filterComponents/BandOptions.js
@@ -1,7 +1,7 @@
 import { useContext } from "react"
 import GlobalContext from "../GlobalContext"
 
-export default function (){
+export default function () {
     const { concerts } = useContext(GlobalContext)
     const bandOptions = concerts.map(a => a.name)
     const uniqueBandOptions = bandOptions.filter((c, index) => {
--- client/src/components/filterComponents/Filter.jsx
+++ client/src/components/filterComponents/Filter.jsx
@@ -2,49 +2,46 @@ import { useContext, useRef, useState } from "react";
 import Eventcard from "../Eventcard.jsx";
 import GlobalContext from "../GlobalContext.jsx";
 import BandOptions from "./BandOptions.js";
+import EndTimestamp from "./EndTimestamp.js";
 import Dropdown from "./FilterDropdown.jsx";
+import StartTimestamp from "./StartTimestamp.js";
 import VenueOptions from "./VenueOptions.js";
 
 export default function () {
     const { concerts } = useContext(GlobalContext)
-    let [sortedArray, setSortedArray] = useState([])
     const [selectedVenue, setSelectedVenue] = useState(null)
     const [selectedBand, setSelectedBand] = useState(null)
     const startDate = useRef(null)
     const endDate = useRef(null)
-    const currentDate = new Date().toJSON().slice(0, 10);
-
-    const selectBand = (band) => {
-        setSelectedBand(band)
-    }
-    const selectVenue = (venue) => {
-        setSelectedVenue(venue)
-    }
+    let [filteredArray, setfilteredArray] = useState([])
 
     function filterArray() {
         const cloneConcerts = [...concerts]
-        console.log(startDate.current.value)
+
         for (let i = cloneConcerts.length - 1; i >= 0; i--) {
-            if (selectedBand &&
+            const concertTimestamp = new Date(cloneConcerts[i].datum).getTime()
+
+            if (startDate && endDate &&
+                concertTimestamp <= StartTimestamp(startDate.current.value) ||
+                concertTimestamp >= EndTimestamp(endDate.current.value)) {
+                cloneConcerts.splice(i, 1)
+            } else if (selectedBand &&
                 cloneConcerts[i].name !== selectedBand) {
                 cloneConcerts.splice(i, 1)
             } else if (selectedVenue &&
                 cloneConcerts[i].venue !== selectedVenue) {
                 cloneConcerts.splice(i, 1)
-            }/* else if (startDate && endDate &&
-                cloneConcerts[i].datum.getTime() !> startDate.getTime() &
-                cloneConcerts[i].datum.getTime() !< endDate.getTime()) {
-                cloneConcerts.splice(i,1) 
-        } */
+            }
         }
-        return setSortedArray(sortedArray = cloneConcerts)
+        cloneConcerts.sort((a, b) => a.datum.localeCompare(b.datum, undefined, { numeric: true }))
+        return setfilteredArray(filteredArray = cloneConcerts)
     }
 
     function DisplayResults() {
         return <div className="card-area-wrapper">
             <div className="card-area">
                 {
-                    sortedArray.map(concert => <Eventcard details={concert}/>)
+                    filteredArray.map(concert => <Eventcard details={concert} />)
                 }
             </div>
         </div>
@@ -52,21 +49,21 @@ export default function () {
 
     function TwoDatesFilter() {
         return <div className="two-dates-filter">
-            <p>Start Date:<input type="date" min={currentDate} defaultValue={currentDate} ref={startDate} /></p>
-            <p>End Date<input type="date" min={currentDate} defaultValue={currentDate} ref={endDate} /></p>
+            <p>Start Date:<input type="date" defaultValue={"2022-09-15"} ref={startDate} /></p>
+            <p>End Date:<input type="date" defaultValue={"2022-09-30"} ref={endDate} /></p>
         </div>
     }
 
     function SearchButton() {
-        return<div class="button-wrap">
-         <button className="search-button" onClick={() => { filterArray() }}>Search</button>
-        </div>    
-}
+        return <div className="button-wrap">
+            <button className="search-button" onClick={() => { filterArray() }}>Search</button>
+        </div>
+    }
 
     return <>
         <div className="filter-container">
-            <Dropdown isSearchable placeHolder="Select Venue..." options={VenueOptions()} selection={selectVenue} />
-            <Dropdown isSearchable placeHolder="Select Band..." options={BandOptions()} selection={selectBand} />
+            <Dropdown isSearchable placeHolder="Select Band..." options={BandOptions()} selection={setSelectedBand} />
+            <Dropdown isSearchable placeHolder="Select Venue..." options={VenueOptions()} selection={setSelectedVenue} />
             <TwoDatesFilter />
             <SearchButton />
         </div>
