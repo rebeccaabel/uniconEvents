007f46f1f6f83235cd759edad3277bd77723fb3e
LinaOlina <liina.oliin@gmail.com>
2023-02-09 16:41:34 +0100

    added mockdata

--- client/src/components/paymentComponents/CardDetails.jsx
+++ client/src/components/paymentComponents/CardDetails.jsx
@@ -2,12 +2,15 @@ import {Link} from "react-router-dom";
 import CalculatePricePaymentpage from "./CalculatePricePaymentpage.jsx";
 import FormPaymentDetails from "./FormPaymentDetails.jsx";
 import ImageArtistHub from "../eventComponents/ImageEventHub.jsx";
+import ImagePaymentpage from "./ImagePaymentpage.jsx";
+import TitlePaymentpage from "./TitlePaymentpage.jsx";
 
 export default function () {
 
     return <div className="payment-wrapper">
         <div className="payment-container">
-            <ImageArtistHub/>
+            <ImagePaymentpage/>
+            <TitlePaymentpage/>
             <div className="payment-details">
                 <CalculatePricePaymentpage/>
                 <FormPaymentDetails/>
