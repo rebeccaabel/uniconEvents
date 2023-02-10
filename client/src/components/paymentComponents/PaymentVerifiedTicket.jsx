c50c10b1f5864ad8dd7f6f5e1a66fc3a1de7260c
Erimara <erimarandersson@gmail.com>
2023-02-10 09:07:04 +0100

    added qr code touch

--- client/src/components/paymentComponents/PaymentVerifiedTicket.jsx
+++ client/src/components/paymentComponents/PaymentVerifiedTicket.jsx
@@ -1,8 +1,5 @@
 import React from "react";
 import "../../styles/PaymentVerifiedTicket.css";
-import TitleArtistHub from "../eventComponents/TitleEventHub.jsx";
-import LocationArtistHub from "../eventComponents/LocationEventHub.jsx";
-import ImageArtistHub from "../eventComponents/ImageEventHub.jsx";
 import CalculatePricePaymentpage from "./CalculatePricePaymentpage.jsx";
 import ImagePaymentpage from "./ImagePaymentpage.jsx";
 import GoogleMaps from "../GoogleMaps.jsx";
@@ -12,7 +9,7 @@ function VerifiedCard() {
                 <h1>Thank you for your purchase!</h1>
             <ImagePaymentpage/>
             <h2>Your Ticket</h2>
-            <img id={"qr-code"}   src="https://api.qrserver.com/v1/create-qr-code/?data=HelloWorld&amp;size=100x100" alt="" title="" />
+            <img id={"qr-code"}   src="https://api.qrserver.com/v1/create-qr-code/?data=https://www.youtube.com/watch?v=KlqpFH3b-c4&amp;size=100x100" alt="" title="" />
                 <CalculatePricePaymentpage/>
             <div className="details">
                 <button id={"print-button"} onClick={printTicket}>Print out ticket</button>
